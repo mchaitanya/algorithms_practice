0092483979c0622f1e58343d3fa039e1cf858803
mchaitanya <chaitanyaaa@gmail.com>
2020-09-20 22:48:50 -0500

    Best Time to Buy and Sell Stock - O(n)

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -4,15 +4,25 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) { 
-    // brute force, O(n^2)
-    let max = 0;
-    for (let i = 0; i < prices.length; i++) {
-        for (let j = i+1; j < prices.length; j++) {
-            if (prices[j] - prices[i] > max) {
-                max = prices[j] - prices[i];
-            }
+var maxProfit = function(prices) {    
+    let minPrice = Number.MAX_VALUE;
+    let maxProfit = 0;
+    for (const price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else if (price - minPrice > maxProfit) {
+            maxProfit = price - minPrice;
         }
     }
-    return max;
+    return maxProfit;
+    
+    // let max = 0;
+    // for (let i = 0; i < prices.length; i++) {
+    //     for (let j = i+1; j < prices.length; j++) {
+    //         if (prices[j] - prices[i] > max) {
+    //             max = prices[j] - prices[i];
+    //         }
+    //     }
+    // }
+    // return max;
 };
