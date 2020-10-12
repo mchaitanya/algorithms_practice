789d62e4752e277ee11e8657e549faaac4f5bd2f
mchaitanya <chaitanyaaa@gmail.com>
2020-10-11 23:53:44 -0500

    Word Break - TLE - 39/43 test cases passed

--- dynamic-programming/word-break.js
+++ dynamic-programming/word-break.js
@@ -6,17 +6,35 @@
  * @return {boolean}
  */
 var wordBreak = function(s, wordDict) {
-    if (s === '') {
-        return true;
-    }
-    
+    const charsS = new Set(s);
+    const charsWordDict = new Set();
     for (let word of wordDict) {
-        // if one of the words matches the start of s & the remainder can be segmented
-        if (s.startsWith(word) && wordBreak(s.slice(word.length), wordDict)) {
-            return true;
+        for (let letter of word) {
+            charsWordDict.add(letter);
         }
     }
     
-    return false;
+    // check if there are any chars in s that don't even occur in wordDict
+    for (let char of charsS) {
+        if (!charsWordDict.has(char)) {
+            return false;
+        }
+    }
+    
+    // check recursively
+    return (function _isSegmentable(s) {
+        if (s === '') {
+            return true;
+        }
+
+        for (let word of wordDict) {
+            // if one of the words matches the start of s & the remainder can be segmented
+            if (s.startsWith(word) && _isSegmentable(s.slice(word.length))) {
+                return true;
+            }
+        }
+
+        return false;
+    })(s);
     
 };
