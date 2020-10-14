724680abee84065eabf7a70d50eeb3a9a0b3d526
mchaitanya <chaitanyaaa@gmail.com>
2020-10-14 18:10:16 -0500

    Try 'prime factor decomposition' approach to check anagrams

--- string/valid-anagram.js
+++ string/valid-anagram.js
@@ -29,4 +29,40 @@ var isAnagram = function(s, t) {
     // check if map is empty
     return map.size === 0;
     
+    // INTERESTING APPROACH - prime factor decomposition
+    // https://yangshun.github.io/tech-interview-handbook/algorithms/string/#anagram
+    // map each letter to a prime number, multiply all the mapped numbers - both strings should result in the same product
+    // fails on the last test case - both products grow too large to fit in a JS number
+
+    // // map each lowercase letter to a prime number
+    // let _primeMap = new Map();
+    // // stop when we find 26 primes - one for each letter
+    // for (let i = 2, count = 0; count < 26; i++) {
+    //     // check i against the primes found so far
+    //     let isPrime = true;
+    //     for (let j = 0; j < count; j++) {
+    //         let char = String.fromCharCode('a'.charCodeAt(0) + j);
+    //         if (_primeMap.has(char) && (i % _primeMap.get(char) === 0)) {
+    //             isPrime = false;
+    //             break; // i is divisible by one of previously found primes
+    //         }
+    //     }
+        
+    //     if (isPrime) {
+    //         let char = String.fromCharCode('a'.charCodeAt(0) + count);
+    //         _primeMap.set(char, i);
+    //         count++;
+    //     }
+        
+    // }
+    
+    // function _computeProduct(str) {
+    //     let product = 1;
+    //     for (let char of str) {
+    //         product = (product * _primeMap.get(char));
+    //     }
+    //     return product;
+    // }
+    
+    // return _computeProduct(s) === _computeProduct(t);
 };
