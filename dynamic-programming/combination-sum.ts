88ef7bdc8e8f5a967327728fb22d5b536a3cfe16
mchaitanya <chaitanyaaa@gmail.com>
2020-10-13 23:32:43 -0500

    Combination Sum

---
+++ dynamic-programming/combination-sum.ts
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/combination-sum/
+// tags - dynamic-programming
+function combinationSum(candidates: number[], target: number): number[][] {
+    // solve recursively with memoization
+    const memo: Map<number, number[][]> = new Map();
+    
+    (function _findCombinations(num: number): number[][] {
+        if (memo.has(num)) {
+            return <number[][]> memo.get(num);
+        }
+        
+        let combinations = [];
+        for (let candidate of candidates) {
+            if (candidate === num) {
+                combinations.push([candidate]);
+            } else if (num > candidate) {
+                for (let sub of _findCombinations(num - candidate)) {
+                    if (sub.length > 0) {
+                        combinations.push([candidate, ...sub]);
+                    }
+                }
+            }
+        }
+        
+        // weed out the duplicates
+        const uniqueCombinations = [];
+        const normalized = new Set();
+        for (let combination of combinations) {
+            let key = combination.sort((n1, n2) => n1 - n2).join('-');
+            if (!normalized.has(key)) {
+                normalized.add(key);
+                uniqueCombinations.push(combination);
+            }
+        }
+        
+        memo.set(num, uniqueCombinations);
+        return uniqueCombinations;
+        
+    })(target);
+    
+    return <number[][]> memo.get(target);
+    
+};
