eda8fffba0b30fb2e030ebf2374e662ff074ee94
mchaitanya <chaitanyaaa@gmail.com>
2020-10-15 16:56:36 -0500

    Critical Connections in a Network - Time Limit Exceeded

--- graph/critical-connections-in-a-network.js
+++ graph/critical-connections-in-a-network.js
@@ -19,30 +19,42 @@ var criticalConnections = function(n, connections) {
         graph.get(connection[0]).push(connection[1]);
         graph.get(connection[1]).push(connection[0]);
     }
+    // console.log(graph);
     
-    console.log(graph);
-    
-    const criticalEdges = [];
-    let newCriticalEdges = [];
-    do {
-        newCriticalEdges = [];
-        for (let [vertex, connectedVertices] of graph.entries()) {
-            if (connectedVertices.length == 1) {
-                newCriticalEdges.push([vertex, connectedVertices[0]]);
-                graph.delete(vertex);
+    // checks if there's a path from v1 to v2 
+    // that doesn't go through the vertices in seen
+    function _doesAlternateConnectionExist(v1, v2) {
+        // BFS from v1 out - iterative implementation
+        const seen = new Set([v2]);
+        let verticesCheck = [v1];
+        while (verticesCheck.length > 0) {
+            let nextVerticesCheck = [];
+            for (let v of verticesCheck) {
+                for (let n of graph.get(v)) {
+                    if (v !== v1 && n === v2) {
+                        return true;
+                    } else if (!seen.has(n)) {
+                        nextVerticesCheck.push(n);
+                        seen.add(n);
+                    }
+                }
             }
+            
+            verticesCheck = nextVerticesCheck;
         }
         
-        // fix the connections in the map
-        for (let edge of newCriticalEdges) {
-            graph.set(edge[1], graph.get(edge[1]).filter(v => v !== edge[0]) )
+        return false;
+        
+    }
+    
+    const critical = [];
+    for (let conn of connections) {
+        // if there's no alternate path between these vertices, then the edge is critical
+        if (!_doesAlternateConnectionExist(conn[0], conn[1])) {
+            critical.push(conn);
         }
-        
-        criticalEdges.push(...newCriticalEdges);
-        
-    } while (newCriticalEdges.length > 0)
+    }
     
-    
-    return criticalEdges;
+    return critical;
     
 };
