7a97c8eef19dddb04e238c7294cd42fab4426543
mchaitanya <chaitanyaaa@gmail.com>
2020-10-16 08:03:43 -0500

    Range Sum of BST

---
+++ tree/range-sum-of-bst.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/range-sum-of-bst/
+// tags - tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    // solve recursively
+    if (root == null) {
+        return 0;
+    }
+    
+    let sum = 0;
+    if (root.val > L) {
+        sum += rangeSumBST(root.left, L, R);
+    }
+    
+    if (root.val >= L && root.val <= R) {
+        sum += root.val;
+    }
+    
+    if (root.val < R) {
+        sum += rangeSumBST(root.right, L, R);
+    }
+    
+    return sum;
+    
+};
