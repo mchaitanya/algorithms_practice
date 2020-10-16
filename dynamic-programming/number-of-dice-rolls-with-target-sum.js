0392aff57ec681df15371b268775d9919b4b09aa
mchaitanya <chaitanyaaa@gmail.com>
2020-10-16 14:02:33 -0500

    Number of Dice Rolls with Target Sum - Accepted

--- dynamic-programming/number-of-dice-rolls-with-target-sum.js
+++ dynamic-programming/number-of-dice-rolls-with-target-sum.js
@@ -12,37 +12,22 @@ var numRollsToTarget = function(d, f, target) {
     const memo = Array(d+1);
     for (let dice = 0; dice < memo.length; dice++) {
         if (dice === 0) {
-            memo[dice] = Array(target+1).fill(0); // base case 1
+            // base case 1 - there aren't any dice
+            memo[dice] = Array(target+1).fill(0);
+        } else if (dice === 1) {
+            // base case 2
+            memo[dice] = Array(target+1).fill(0).map((_, t) => t >= 1 && t <= f ? 1: 0);
         } else {
-            memo[dice] = Array(target+1).fill(undefined)
-                .map((_, t) => {
-                    if (t === 0) {
-                        return 0;
-                    } else if (t >= 1 && t <= f) { // base case 2
-                        return 1;
-                    } else {
-                        return undefined;
-                    }
-                });
+            memo[dice] = Array(target+1).fill(undefined);
+            memo[dice][0] = 0; // result is 0 if target is 0
         }
     }
     
-    let prod = 1;
-    for (let i = 0; i < 9; i++) {
-        prod *= 10;
-    }
-    const base = prod + 7;
+    const base = 10**9 + 7;
     // console.log(base);
     // (a + b + c) % n = ((a%n) + (b%n) + (c%n)) % n
     
     (function _countRolls(d, target) {
-        // // base cases
-        // if (d === 0) {
-        //     return 0;
-        // } else if (d >= 1 && target >= 1 && target <= f) {
-        //     return 1;
-        // }
-        
         // check the memo array
         if (memo[d][target] !== undefined) {
             return memo[d][target];
@@ -52,7 +37,7 @@ var numRollsToTarget = function(d, f, target) {
         // we can roll any number with this die
         for (let roll = 1; roll <= f; roll++) {
             if (target > roll) {
-                numRolls += _countRolls(d-1, target - roll);
+                numRolls += (_countRolls(d-1, target - roll) % base);
             }
         }
 
