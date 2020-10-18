2fa7ee3a394be57dd1d98ebf15470297c4231b47
mchaitanya <chaitanyaaa@gmail.com>
2020-10-18 08:05:14 -0500

    Pow(x,n) - accepted

--- math/powx-n.js
+++ math/powx-n.js
@@ -1,20 +1,44 @@
 // https://leetcode.com/problems/powx-n/
-// tags- math
+// tags- math, recursion, dynamic-programming
 /**
  * @param {number} x
  * @param {number} n
  * @return {number}
  */
 var myPow = function(x, n) {
-    if (x === 0) {
-        return 1;
-    }
+    // calculate recursively with memoization
+    const memo = new Map();
+    memo.set(0, 1);
     
-    let result = 1;
-    for (let i = 0, end = Math.abs(n); i < end; i++) {
-        result *= x;
-    }
+    const abs = Math.abs(n);
+    (function _calculatePower(n) {
+        if (memo.has(n)) {
+            return memo.get(n);
+        }
+        
+        let result = 1;
+        if (n % 2 === 0) {
+            result = _calculatePower(n/2) * _calculatePower(n/2);
+        } else {
+            result = x * _calculatePower(n-1);
+        }
+        
+        memo.set(n, result);
+        return result;
+        
+    })(abs);
     
-    return n > 0 ? result : (1/result);
+    return n > 0 ? memo.get(abs) : (1/memo.get(abs));
+    
+//     if (x === 0) {
+//         return 1;
+//     }
+    
+//     let result = 1;
+//     for (let i = 0, end = Math.abs(n); i < end; i++) {
+//         result *= x;
+//     }
+    
+//     return n > 0 ? result : (1/result);
     
 };
