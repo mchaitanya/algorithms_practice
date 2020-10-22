5e1d764c59e1bfec2edb391404b704b326b63446
mchaitanya <chaitanyaaa@gmail.com>
2020-10-21 23:37:14 -0500

    Prison Cells After N Days - accepted

--- array/prison-cells-after-n-days.js
+++ array/prison-cells-after-n-days.js
@@ -6,10 +6,11 @@
  * @return {number[]}
  */
 var prisonAfterNDays = function(cells, N) {
-    // we are told that there are 8 cells & really only the inner 6 cells change with time
+    // we are told that there are 8 cells 
     const NUM_CELLS = 8;
+    // TRY - with bitmask
     function _convertStateToInt(state) {
-        const binaryStr = state.slice(1, NUM_CELLS-1).join('');
+        const binaryStr = state.join('');
         return parseInt(binaryStr, 2);
     }
     
@@ -22,49 +23,44 @@ var prisonAfterNDays = function(cells, N) {
     }
     
     // detect when a cycle occurs
-    // total combination = 2^6 = 64 - so at some point the state starts to repeat itself
-    const seen = Array(2**(NUM_CELLS-2)).fill(false);
+    // total combinations = 2^8 = 256 - so at some point the state starts to repeat itself
+    const daySeen = Array(2**NUM_CELLS).fill(undefined);
     const states = [];
     
-    let state = cells;
     let day = 0;
+    let state = cells;
+    let cycleStart = 0, cycleEnd = 0;
     for (; day < N; day++) {
         const key = _convertStateToInt(state);
-        if (seen[key]) {
+        if (daySeen[key] !== undefined) {
+            cycleStart = daySeen[key];
+            cycleEnd = day;
             break; // cycle detected
-        } else if (state[0] === 0 && state[NUM_CELLS-1] === 0) {
-            // both the cells at the end have turned vacant - this'll take at most one turn
+        } else {
             states.push(state);
-            seen[key] = true;
+            daySeen[key] = day;
         }
-
+        
         let newState = Array(NUM_CELLS).fill(0);
         for (let i = 1; i < NUM_CELLS - 1; i++) {
             if ((state[i-1] === 1 && state[i+1] == 1) || (state[i-1] === 0 && state[i+1] === 0)) {
                 newState[i] = 1;
             }
         }
+        
         state = newState;
     }
     
     if (day === N) {
         return state;
     } else {
-        // we detected a cycle & exited early, let's say day = k at this point
-        // states should be an array of size k or k-1
-        console.log('day = ' + day);
-        for (let i = 0; i < states.length; i++) {
-            _printPrison(states[i]);
-        }
+        // we detected a cycle & exited early
+        // console.log('day = ' + day);
+        // for (let i = 0; i < states.length; i++) {
+        //     _printPrison(states[i]);
+        // }
         
-        // states[i] is the state at the start of day i
-        // so state after day i is states[i+1]
-        if (states.length === day) {
-            // both the cells are the end were vacant at the start
-            return states[N+1 % day];
-        } else {
-            return states[N % day];
-        }
+        return states[cycleStart + (N - cycleStart) % (cycleEnd - cycleStart)];
         
     }
     
