8b1b4cf3e80d77724f031f05b5b0a554ce977852
mchaitanya <chaitanyaaa@gmail.com>
2020-10-23 07:43:51 -0500

    Diameter of N-Ary Tree

---
+++ tree/diameter-of-n-ary-tree.js
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/diameter-of-n-ary-tree/
+// tags - tree
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+var diameter = function(root) {
+    let diam = 0;
+    (function _getMaxNodesToLeaf(node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        let max = 0;
+        let secondMax = 0;
+        for (let c of node.children) {
+            let n = _getMaxNodesToLeaf(c);
+            if (n > max) {
+                secondMax = max;
+                max = n;
+            } else if (n > secondMax) {
+                secondMax = n;
+            }
+        }
+        
+        // console.log('Checking ' + node.val + ': ' + max + ', ' + secondMax);
+        
+        // update the diameter
+        if (max + secondMax > diam) {
+            diam = max + secondMax;
+        }
+        
+        return max + 1;
+        
+    })(root)
+    
+    return diam;
+    
+};
