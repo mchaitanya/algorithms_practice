a5c9b87c66a641b29e46140671d48a739dba2962
mchaitanya <chaitanyaaa@gmail.com>
2020-10-30 00:24:04 -0500

    Reverse Bits - alternate solution
    This also uses parseInt - this treat the binary string as an unsigned integer.

--- binary/reverse-bits.js
+++ binary/reverse-bits.js
@@ -9,21 +9,22 @@ var reverseBits = function(n) {
     //     console.log(n.toString(2));
     // }
     
-    // // we are given there'll be 32 bits
-    // let reversed = 0;
-    // // _printBinary(reversed);
-    // for (let i = 0, j = 31; i < 32; i++, j--) {
-    //     if ((n & (1 << i)) !== 0) {
-    //         // if the ith bit is 1, then set the jth bit to 1 in the result
-    //         reversed |= (1 << j);
-    //     }
-    //     // _printBinary(reversed);
-    // }
-    // return reversed;
+    // we are given there'll be 32 bits
+    let arr = Array(32).fill('0');
+    // _printBinary(reversed);
+    for (let i = 0; i < 32; i++) {
+        if ((n & (1 << i)) !== 0) {
+            // if the ith bit is 1, then set the jth bit to 1 in the result
+            // reversed |= (1 << j);
+            arr[i] = '1';
+        }
+        // _printBinary(reversed);
+    }
+    return parseInt(arr.join(''), 2);
     
-    let str = n.toString(2).padStart(32, '0');
-    let reversed = str.split('').reverse().join('');
+//     let str = n.toString(2).padStart(32, '0');
+//     let reversed = str.split('').reverse().join('');
     
-    return parseInt(reversed, 2)
+//     return parseInt(reversed, 2);
     
 };
