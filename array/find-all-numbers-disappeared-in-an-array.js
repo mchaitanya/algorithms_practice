f4b682c5caa960c30fdd08c4d8f21a8c09faa953
mchaitanya <chaitanyaaa@gmail.com>
2020-11-02 16:20:16 -0600

    Find All Numbers Disappeared in an Array

---
+++ array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+// tags - array
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    //     // some appear twice, some once, some not at all
+    //     const counts = Array(nums.length).fill(0);
+    //     for (let n of nums) {
+    //         counts[n-1]++;
+    //     }
+        
+    //     return counts.reduce((result, count, i) => {
+    //         if (count === 0) {
+    //             return result.concat(i+1);
+    //         } else {
+    //             return result;
+    //         };
+    //     }, []);
+        
+        const isAbsent = Array(nums.length).fill(true);
+        for (let n of nums) {
+            isAbsent[n-1] = false;
+        }
+        
+        return isAbsent.reduce((result, flag, i) => flag ? result.concat(i+1) : result, []);
+        
+    };
