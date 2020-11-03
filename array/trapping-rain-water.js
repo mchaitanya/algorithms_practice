ab0a96bb1b1d961a48defd5b2d711c5ce08f1c9e
mchaitanya <chaitanyaaa@gmail.com>
2020-11-02 21:03:36 -0600

    Trapping Rain Water - refactor
    Remove else block - move code to zero-indent level in function body

--- array/trapping-rain-water.js
+++ array/trapping-rain-water.js
@@ -9,23 +9,22 @@ var trap = function(height) {
     function _getWater(left, heights, right) {
         if (heights.length === 0) {
             return 0;
+        }
+
+        // find the max height
+        let split = 0;
+        for (let i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[split]) {
+                split = i;
+            }
+        }
+        
+        let low = Math.min(left, right);
+        if (heights[split] < low) {
+            return heights.reduce((water, h) => water+low-h, 0);
         } else {
-            // find the max height
-            let split = 0;
-            for (let i = 1; i < heights.length; i++) {
-                if (heights[i] > heights[split]) {
-                    split = i;
-                }
-            }
-            
-            let low = Math.min(left, right);
-            if (heights[split] < low) {
-                return heights.reduce((water, h) => water+low-h, 0);
-            } else {
-                return _getWater(left, heights.slice(0,split), heights[split])
-                    + _getWater(heights[split], heights.slice(split+1), right);
-            }
-            
+            return _getWater(left, heights.slice(0,split), heights[split])
+                + _getWater(heights[split], heights.slice(split+1), right);
         }
         
     }
