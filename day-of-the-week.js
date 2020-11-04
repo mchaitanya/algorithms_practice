b8f2e55339f2dca5b138c67886d9399932a3b4fa
mchaitanya <chaitanyaaa@gmail.com>
2020-11-04 14:30:32 -0600

    Day of the Week
    Takes advantage of the standard JS Date object.

---
+++ day-of-the-week.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/day-of-the-week/
+// tags - date
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+    const days = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednesday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday'
+    ];
+    
+    const date = new Date(year, month-1, day);
+    return days[date.getDay()];
+};
