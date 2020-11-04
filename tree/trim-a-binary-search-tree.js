5842b508c590befc023d34e3cb850bfdde887740
mchaitanya <chaitanyaaa@gmail.com>
2020-11-04 11:37:05 -0600

    Trim a Binary Search Tree

---
+++ tree/trim-a-binary-search-tree.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/trim-a-binary-search-tree/
+// tags - tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {TreeNode}
+ */
+var trimBST = function(root, low, high) {
+    // solve recursively
+    if (root == null) {
+        return null;
+    } else if (root.val < low) {
+        return trimBST(root.right, low, high);
+    } else if (root.val > high) {
+        return trimBST(root.left, low, high);
+    }
+    
+    root.left = trimBST(root.left, low, high);
+    root.right = trimBST(root.right, low, high);
+    
+    return root;
+    
+};
