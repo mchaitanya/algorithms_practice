429d4aaff39c4c1ee2e7d15f62f53cde6d01b37f
mchaitanya <chaitanyaaa@gmail.com>
2020-11-14 12:37:50 -0600

    Resolve 'Merge Two Sorted Lists'

--- linked-list/merge-two-sorted-lists.js
+++ linked-list/merge-two-sorted-lists.js
@@ -13,30 +13,62 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let mergedHead = null;
-    let currentNode = null;
-    let currentNode1 = l1;
-    let currentNode2 = l2;
-    while (currentNode1 != null || currentNode2 != null) {
-        let smallerNode = null;
-        if (currentNode2 == null || (currentNode1 != null && (currentNode1.val <= currentNode2.val))) {
-            smallerNode = currentNode1;
-            currentNode1 = currentNode1.next;
-        } else if (currentNode1 == null || (currentNode2 != null && (currentNode2.val <= currentNode1.val))) {
-            smallerNode = currentNode2;
-            currentNode2 = currentNode2.next;
-        }
+    // let mergedHead = null;
+    // let currentNode = null;
+    // let currentNode1 = l1;
+    // let currentNode2 = l2;
+    // while (currentNode1 != null || currentNode2 != null) {
+    //     let smallerNode = null;
+    //     if (currentNode2 == null || (currentNode1 != null && (currentNode1.val <= currentNode2.val))) {
+    //         smallerNode = currentNode1;
+    //         currentNode1 = currentNode1.next;
+    //     } else if (currentNode1 == null || (currentNode2 != null && (currentNode2.val <= currentNode1.val))) {
+    //         smallerNode = currentNode2;
+    //         currentNode2 = currentNode2.next;
+    //     }
         
-        if (mergedHead == null) {
-            mergedHead = smallerNode;
+    //     if (mergedHead == null) {
+    //         mergedHead = smallerNode;
+    //     } else {
+    //         currentNode.next = smallerNode;
+    //     }
+        
+    //     currentNode = smallerNode;
+        
+    // }
+    
+    // return mergedHead;
+
+    let curr = null, merged = null;
+    let n1 = l1, n2 = l2;
+    while (n1 != null || n2 != null) {
+        // there's at least one node left in either of the lists
+        // -100 <= Node.val <= 100
+        let val1 = n1?.val ?? 101;
+        let val2 = n2?.val ?? 101;
+        
+        let smaller = null;
+        if (val1 < val2) {
+            // console.log([val1, val2], val1);
+            smaller = n1;
+            n1 = n1.next;
         } else {
-            currentNode.next = smallerNode;
+            // console.log([val1, val2], val2);
+            smaller = n2;
+            n2 = n2.next;
         }
         
-        currentNode = smallerNode;
+        
+        if (merged == null) {
+            merged = smaller;
+        } else {
+            curr.next = smaller;
+        }
+        
+        curr = smaller;
         
     }
     
-    return mergedHead;
+    return merged;
     
 };
