5d45fd3f6d66dffa1035a3476d5f0af3fe929365
mchaitanya <chaitanyaaa@gmail.com>
2020-11-19 17:40:48 -0600

    Re-solve Longest Substring Without Repeating Characters
    Solve in O(n) with a sliding window.

--- string/longest-substring-without-repeating-characters.js
+++ string/longest-substring-without-repeating-characters.js
@@ -5,41 +5,67 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    //     // search greedily from the left
-    //     // fails on "dvdf" - greedy approach won't work
-    //     let maxLen = 0;
-    //     const set = new Set();
-    //     for (let i = 0; i < s.length; i++) {
-    //         set.add(s[i]);
-    //         if (i+1 == s.length || set.has(s[i+1])) {
-    //             if (set.size > maxLen) {
-    //                 maxLen = set.size;
-    //             }
-    //             set.clear();
-    //         }
-            
+//     // search greedily from the left
+//     // fails on "dvdf" - greedy approach won't work
+//     let maxLen = 0;
+//     const set = new Set();
+//     for (let i = 0; i < s.length; i++) {
+//         set.add(s[i]);
+//         if (i+1 == s.length || set.has(s[i+1])) {
+//             if (set.size > maxLen) {
+//                 maxLen = set.size;
+//             }
+//             set.clear();
+//         }
+        
+//     }
+//     return maxLen;
+    
+//     if (s.length === 0) {
+//         return 0;
+//     }
+    
+//     // apply dynamic programming - state[i] = length of LSWRC ending on i
+//     const state = Array(s.length).fill(1);
+//     for (let i = 1; i < s.length; i++) {
+//         let j = 0;
+//         while (j < state[i-1]) {
+//             if (s[i-j-1] === s[i]) {
+//                 break;
+//             }
+//             j++;
+//         }
+//         state[i] += j;
+//     }
+    
+//     return Math.max(...state);
+    
+    
+    // let maxLen = 0;
+    // for (let i = 0, sub = ''; i < s.length; i++) {
+    //     sub += s[i];
+    //     if (i === s.length-1 || sub.includes(s[i+1])) {
+    //         maxLen = Math.max(maxLen, sub.length);
+    //         sub = sub.slice(sub.indexOf(s[i+1]) + 1);
     //     }
-    //     return maxLen;
-        
-        if (s.length === 0) {
-            return 0;
+    // }
+    // return maxLen;
+    
+    
+    // using a HashSet as a sliding window
+    let maxLen = 0;
+    let start = 0, end = 0;
+    let window = new Set();
+    while (start < s.length && end < s.length) {
+        if (!window.has(s[end])) {
+            window.add(s[end]);
+            end++;
+            maxLen = Math.max(maxLen, end-start);
+        } else {
+            // we keep removing chars from the start till there are no duplicates
+            window.delete(s[start]);
+            start++;
         }
-        
-        // apply dynamic programming - state[i] = length of LSWRC ending on i
-        // if we define state[i] = length of LSWRC in substring upto i
-        // then it's hard to define a relation between state[i+1] & state[i]
-        const state = Array(s.length).fill(1);
-        for (let i = 1; i < s.length; i++) {
-            let j = 0;
-            while (j < state[i-1]) {
-                if (s[i-j-1] === s[i]) {
-                    break;
-                }
-                j++;
-            }
-            state[i] += j;
-        }
-        
-        return Math.max(...state);
-        
-    };
+    }
+    return maxLen;
+};
