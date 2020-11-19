fb628484b0f7d2e8d8ac7778244b915fac93b3f8
mchaitanya <chaitanyaaa@gmail.com>
2020-11-19 09:22:42 -0600

    Reverse Words in a String III - apply pattern to segment string

--- string/reverse-words-in-a-string-iii.js
+++ string/reverse-words-in-a-string-iii.js
@@ -5,9 +5,25 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return (
-        s.split(' ') // break into words
-         .map(w => w.split('').reverse().join('')) // reverse each word
-         .join(' ') // join them back
-    );
+    // return (
+    //     s.split(' ') // break into words
+    //      .map(w => w.split('').reverse().join('')) // reverse each word
+    //      .join(' ') // join them back
+    // );
+
+    let result = '';
+    for (let i = 0, word = []; i < s.length; i++) {
+        if (s[i] === ' ') {
+            result += ' ';
+            continue;
+        }
+        
+        word.push(s[i]);
+        if (i === s.length-1 || s[i+1] === ' ') {
+            result += word.reverse().join('');
+            word = [];
+        }
+    }
+    return result;
+    
 };
