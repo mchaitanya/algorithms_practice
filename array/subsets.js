0cd6380e83cbf1afe749c7981682e59bc5cb5bf8
mchaitanya <chaitanyaaa@gmail.com>
2020-11-22 14:59:36 -0600

    Re-solve 'Subsets' - complete search method from Laaksonen

--- array/subsets.js
+++ array/subsets.js
@@ -5,13 +5,31 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    // if there are n numbers, there can be 2**n subsets
-    // for each number, decide whether to keep it or not
-    const numSubsets = 2**(nums.length);
-    const result = Array(numSubsets);
-    for (let i = 0; i < numSubsets; i++) {
-        let bitmask = i.toString(2).padStart(nums.length, '0'); 
-        result[i] = nums.filter((_, i) => bitmask[i] === '1');
+    // // if there are n numbers, there can be 2**n subsets
+    // // for each number, decide whether to keep it or not
+    // const numSubsets = 2**(nums.length);
+    // const result = Array(numSubsets);
+    // for (let i = 0; i < numSubsets; i++) {
+    //     let bitmask = i.toString(2).padStart(nums.length, '0'); 
+    //     result[i] = nums.filter((_, i) => bitmask[i] === '1');
+    // }
+    // return result;
+
+
+    // See Laaksonen, Ch5 - Complete Search - Generating Subsets Method 1
+    const result = [];
+    function _search(ix, subset = []) {
+        if (ix === nums.length) {
+            // all elements have been processed & a subset has been generated
+            result.push(subset);
+        } else {
+            // decide whether to include element at ix or not
+            _search(ix+1, subset); // chose not to include
+            _search(ix+1, subset.concat(nums[ix])); // chose to include
+        }
     }
+    
+    _search(0);
     return result;
+    
 };
