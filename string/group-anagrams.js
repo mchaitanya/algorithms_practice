7f2c84779abceb65622567851e688564d2ecc8cc
mchaitanya <chaitanyaaa@gmail.com>
2020-11-25 07:50:48 -0600

    Improve solution to 'Group Anagrams'
    Use a labeled continue statement to check next number if current number isn't prime.

--- string/group-anagrams.js
+++ string/group-anagrams.js
@@ -14,39 +14,30 @@ var groupAnagrams = function(strs) {
         
     //     return Array.from(map.values());
         
-        let primes = [];
-        for (let count = 0, num = 2; count < 26; num++) {
-            let isPrime = true;
-            // check divisibility against the primes found so far
-            for (let p of primes) {
-                if (num % p === 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            
-            if (isPrime) {
-                primes.push(num);
-                count++;
+    let primeMap = new Map();
+    search:
+    for (let count = 0, num = 2, code = 'a'.charCodeAt(0); count < 26; num++) {
+        // check divisibility against the primes found so far
+        for (let p of primeMap.values()) {
+            if (num % p === 0) {
+                continue search;
             }
         }
         
-        // console.log(primes);
-        
-        let primeMap = new Map();
-        for (let i = 0, code = 'a'.charCodeAt(0); i < 26; i++, code++) {
-            primeMap.set(String.fromCharCode(code), primes[i]);
-        }
-        
-        // console.log(primeMap);
-        
-        let anagramMap = new Map();
-        for (let str of strs) {
-            const key = str.split('').reduce((prod, char) => prod * primeMap.get(char), 1);
-            const anagrams = anagramMap.get(key) || [];
-            anagramMap.set(key, anagrams.concat(str));
-        }
-        
-        return Array.from(anagramMap.values());
-        
-    };
+        // num is a prime if we reach here
+        primeMap.set(String.fromCharCode(code), num);
+        count++;
+        code++;
+    }
+    // console.log(primeMap);
+    
+    let anagramMap = new Map();
+    for (let str of strs) {
+        const key = str.split('').reduce((prod, char) => prod * primeMap.get(char), 1);
+        const anagrams = anagramMap.get(key) || [];
+        anagramMap.set(key, anagrams.concat(str));
+    }
+    
+    return Array.from(anagramMap.values());
+    
+};
