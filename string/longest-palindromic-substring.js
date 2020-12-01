fbd2b1b126bafb6254afe0b05638858e48cd18d0
mchaitanya <chaitanyaaa@gmail.com>
2020-12-01 11:50:15 -0600

    Longest Palindromic Substring - apply 'expand around center' idea

--- string/longest-palindromic-substring.js
+++ string/longest-palindromic-substring.js
@@ -5,30 +5,38 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    if (s === '') {
-        return '';
-    }
+//     if (s === '') {
+//         return '';
+//     }
     
-    function _isPalindrome(start, end) {
-        for (let i = start, j = end-1; i < j; i++, j--) {
-            if (s[i] !== s[j]) {
-                return false;
-            }
-        }
-        return true;
-    }
+//     function _isPalindrome(start, end) {
+//         for (let i = start, j = end-1; i < j; i++, j--) {
+//             if (s[i] !== s[j]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
     
-    function _findPalindrome(size) {
-        for (let i = 0, j = i+size; j <= s.length; i++, j++) {
-            if (_isPalindrome(i,j)) {
-                return s.slice(i,j);
-            }
-        }
-        return null;
-    }
-
-    // // binary search the solution space - won't work as-is
-    // // if there's no palindrome of length 2, can't say there isn't one of length 3
+//     function _findPalindrome(size) {
+//         for (let i = 0, j = i+size; j <= s.length; i++, j++) {
+//             if (_isPalindrome(i,j)) {
+//                 return s.slice(i,j);
+//             }
+//         }
+//         return null;
+//     }
+    
+//     // brute force - O(n^3)
+//     for (let size = s.length; size >= 1; size--) {
+//         const temp = _findPalindrome(size);
+//         if (temp != null) {
+//             return temp;
+//         }
+//     }
+    
+    
+    // // binary search the solution space - even you get it working, it'll be O(n^2logn)
     // let result = '';
     // let low = 1, high = s.length-1;
     // while (low <= high) {
@@ -44,11 +52,37 @@ var longestPalindrome = function(s) {
     // }
     // return result;
     
-    for (let size = s.length; size >= 1; size--) {
-        const temp = _findPalindrome(size);
-        if (temp != null) {
-            return temp;
+    
+    // expand around center
+    function _getLongestPalindromeAround(left, right) {
+        // assume we send a valid palindrome in here - check if it can be extended
+        if (left-1 < 0 || right+1 >= s.length || s[left-1] !== s[right+1]) {
+            return s.substring(left, right+1); // both left and right are inclusive
+        } else {
+            return _getLongestPalindromeAround(left-1, right+1);
         }
     }
+    
+    function _selectLongest(...strs) {
+        return strs.reduce((longest, curr) => {
+            return (curr.length > longest.length ? curr : longest);
+        }, '');
+    }
+    
+    let longest = '';
+    for (let i = 0; i < s.length; i++) {
+        // odd palindrome
+        let po = _getLongestPalindromeAround(i, i);
+        // even palindrome
+        let pe = '';
+        if (i < s.length-1 && s[i] === s[i+1]) {
+            pe = _getLongestPalindromeAround(i, i+1);
+        }
+        
+        longest = _selectLongest(longest, po, pe);
+        
+    }
+    
+    return longest;
     
 };
