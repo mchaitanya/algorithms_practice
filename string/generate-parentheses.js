610ef3b3c7cd20695c6f5eb8c770794136f3254a
mchaitanya <chaitanyaaa@gmail.com>
2020-12-14 12:28:00 -0600

    Re-solve 'Generate Parentheses'
    Apply dynamic programming based on recursive definition of a balance bracket sequence.

--- string/generate-parentheses.js
+++ string/generate-parentheses.js
@@ -9,25 +9,53 @@ var generateParenthesis = function(n) {
     // strategy 1 - generate all strings, then check each to see if it's valid
     // won't work if n = 8, 2^16 - exponential
     
-    // something like bfs?
-    let strs = ['('];
-    for (let i = 1; i < 2*n; i++) {
-        // each pass through the loop, we grow the size of each string by 1
-        let newStrs = [];
-        for (let str of strs) {
-            const numLeft = str.split('').filter(c => c === '(').length;
-            const numRight = str.length - numLeft;
-            // we can add either a '(' or a ')'
-            if (n > numLeft) {
-                newStrs.push(str + '(');
-            }
+//     // something like bfs?
+//     let strs = ['('];
+//     for (let i = 1; i < 2*n; i++) {
+//         // each pass through the loop, we grow the size of each string by 1
+//         let newStrs = [];
+//         for (let str of strs) {
+//             const numLeft = str.split('').filter(c => c === '(').length;
+//             const numRight = str.length - numLeft;
+//             // we can add either a '(' or a ')'
+//             if (n > numLeft) {
+//                 newStrs.push(str + '(');
+//             }
             
-            if (numLeft > numRight) {
-                newStrs.push(str + ')');
+//             if (numLeft > numRight) {
+//                 newStrs.push(str + ')');
+//             }
+//         }
+//         strs = newStrs;
+//     }
+//     return strs;
+    
+    
+    // dynamic programming
+    const memo = Array(n+1).fill(null);
+    memo[0] = [''];
+    function _createSeqs(size) {
+        if (memo[size] != null) {
+            return memo[size];
+        }
+
+        const result = new Set();
+        for (let sub of _createSeqs(size-1)) {
+            result.add('(' + sub + ')');
+        }
+
+        for (let i = 1; i < size; i++) {
+            for (let left of _createSeqs(i)) {
+                for (let right of _createSeqs(size-i)) {
+                    result.add(left + right);
+                }
             }
         }
-        strs = newStrs;
+        
+        memo[size] = Array.from(result);
+        return memo[size];
     }
     
-    return strs;
+    return _createSeqs(n);
+    
 };
