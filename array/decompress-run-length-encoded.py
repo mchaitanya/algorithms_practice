fc23d43891a0db3632026791ee257badce8dc62f
mchaitanya <chaitanyaaa@gmail.com>
2021-06-25 00:16:35 -0700

    Refactor while loop to for

--- array/decompress-run-length-encoded.py
+++ array/decompress-run-length-encoded.py
@@ -2,14 +2,12 @@
 # tags - array
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
-        # decompressed = []
-        # i = 0
-        # while i+1 < len(nums):
-        #     decompressed.extend([nums[i+1]] * nums[i])
-        #     i += 2
-        # return decompressed
-        
         decompressed = []
-        for frequency, value in zip(nums[::2], nums[1::2]):
-            decompressed.extend([value] * frequency)
+        for i in range(0, len(nums), 2):
+            decompressed.extend([nums[i+1]] * nums[i])
         return decompressed
+        
+        # decompressed = []
+        # for frequency, value in zip(nums[::2], nums[1::2]):
+        #     decompressed.extend([value] * frequency)
+        # return decompressed
