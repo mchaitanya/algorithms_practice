fea3e8c9e57acd674fa658b052461a0ebf6cd6d2
mchaitanya <chaitanyaaa@gmail.com>
2023-02-02 22:55:46 +0530

    Count Univalue Subtrees

---
+++ tree/count-univalue-subtrees.js
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/count-univalue-subtrees
+// tags - tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countUnivalSubtrees = function (root) {
+  if (root == null) return 0;
+  let count = 0;
+  // Return whether the subtree rooted at node is univalued.
+  function dfs(node) {
+    // node shouldn't be null.
+    let isUnival = true;
+    if (node.left != null) {
+      const isLeftUnival = dfs(node.left);
+      isUnival &&= node.val === node.left.val && isLeftUnival;
+    }
+    if (node.right != null) {
+      const isRightUnival = dfs(node.right);
+      isUnival &&= node.val === node.right.val && isRightUnival;
+    }
+    if (isUnival) count++;
+    return isUnival;
+  }
+  dfs(root);
+  return count;
+};
