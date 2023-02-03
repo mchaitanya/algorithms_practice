53be5de7001a6251e5319334fe8547689c281a39
mchaitanya <chaitanyaaa@gmail.com>
2023-02-03 11:26:17 +0530

    Decode the Message - map space to itself

--- string/decode-the-message.js
+++ string/decode-the-message.js
@@ -7,16 +7,14 @@
  */
 var decodeMessage = function (key, message) {
   const letters = "abcdefghijklmnopqrstuvwxyz";
-  const mapping = new Map();
+  const mapping = new Map([[" ", " "]]);
   for (let i = 0, j = 0; i < key.length && j < 26; i++) {
-    if (key[i] !== " " && !mapping.has(key[i])) {
+    if (!mapping.has(key[i])) {
       mapping.set(key[i], letters[j++]);
     }
   }
-
-  let decoded = "";
-  for (const char of message) {
-    decoded += char === " " ? " " : mapping.get(char);
-  }
-  return decoded;
+  return message
+    .split("")
+    .map((char) => mapping.get(char))
+    .join("");
 };
