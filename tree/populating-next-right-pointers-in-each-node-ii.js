40bb742141e0b934bdcc0d95859bae023d493d06
mchaitanya <chaitanyaaa@gmail.com>
2023-02-12 20:42:00 +0530

    Populating Next Right Pointers in Each Node II - O(1) space from LC

--- tree/populating-next-right-pointers-in-each-node-ii.js
+++ tree/populating-next-right-pointers-in-each-node-ii.js
@@ -14,30 +14,45 @@
  * @param {Node} root
  * @return {Node}
  */
-var connect = function(root) {
-    // level order traversal seems the most natural way to do this
-    if (root == null) {
-        return null;
-    }
-    
-    let level = [root];
-    while (level.length > 0) {
-        const nextLevel = [];
-        for (let i = 0; i < level.length; i++) {
-            // set the next pointer
-            level[i].next = (i < level.length-1 ? level[i+1] : null);
-            // populate the next level
-            if (level[i].left != null) {
-                nextLevel.push(level[i].left);
-            }
-            
-            if (level[i].right != null) {
-                nextLevel.push(level[i].right);
-            }
-            
+var connect = function (root) {
+  // O(1) space from LC
+  let first = root;
+  // first is the first node on the current level.
+  // This loop iterates over the levels in the tree.
+  while (first != null) {
+    let node = first;
+    // prev & curr are nodes on the next level.
+    let prev = null,
+      curr = null;
+    // If there are any nodes on the next level, first will be set to the first non-null encountered.
+    first = null;
+    // This loop iterates over the nodes in the current level.
+    while (node != null) {
+      for (const child of [node.left, node.right]) {
+        if (child != null) {
+          prev = curr;
+          curr = child;
+          if (prev != null) prev.next = curr;
+          if (prev == null) first = curr;
         }
-        level = nextLevel;
+      }
+      node = node.next;
     }
-    
-    return root;
+  }
+  return root;
+
+  // // Solve with level-order traversal.
+  // if (root == null) return null;
+  // let level = [root];
+  // while (level.length > 0) {
+  //     const nextLevel = [];
+  //     for (let i = 0; i < level.length; i++) {
+  //         const node = level[i];
+  //         if (i < level.length-1) node.next = level[i+1];
+  //         if (node.left != null) nextLevel.push(node.left);
+  //         if (node.right != null) nextLevel.push(node.right);
+  //     }
+  //     level = nextLevel;
+  // }
+  // return root;
 };
