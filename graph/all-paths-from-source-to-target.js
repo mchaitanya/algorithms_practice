3cd85024ff4e3898c70a0bdfdd02297736172a62
mchaitanya <chaitanyaaa@gmail.com>
2023-02-15 13:53:38 +0530

    All Paths From Source to Target - solve with BFS

--- graph/all-paths-from-source-to-target.js
+++ graph/all-paths-from-source-to-target.js
@@ -7,15 +7,31 @@
 var allPathsSourceTarget = function (graph) {
   const n = graph.length;
   const paths = [];
-  // Given graph is a DAG i.e. no cycles. Don't have to keep track of seen nodes.
-  function dfs(v, path) {
-    path.push(v);
-    if (v === n - 1) paths.push([...path]);
-    for (const vn of graph[v]) {
-      dfs(vn, path);
+
+  // Solve with BFS.
+  const queue = [[0]]; // Store the paths in the queue.
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const v = path[path.length - 1];
+    if (v === n - 1) {
+      paths.push(path);
+      continue;
     }
-    path.pop();
+    for (const vn of graph[v]) {
+      queue.push([...path, vn]);
+    }
   }
-  dfs(0, []);
+
+  //   // Given graph is a DAG i.e. no cycles. Don't have to keep track of seen nodes.
+  //   function dfs(v, path) {
+  //     path.push(v);
+  //     if (v === n - 1) paths.push([...path]);
+  //     for (const vn of graph[v]) {
+  //       dfs(vn, path);
+  //     }
+  //     path.pop();
+  //   }
+  //   dfs(0, []);
+
   return paths;
 };
