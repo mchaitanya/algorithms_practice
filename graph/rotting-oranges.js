01be84184497df39d7d230923dfa9875c0d4354c
mchaitanya <chaitanyaaa@gmail.com>
2023-02-15 15:00:44 +0530

    Re-solve "Rotting Oranges"

--- graph/rotting-oranges.js
+++ graph/rotting-oranges.js
@@ -4,61 +4,56 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var orangesRotting = function(grid) {
-    const rcount = grid.length;
-    const ccount = grid[0].length;
-    const EMPTY = 0, FRESH = 1, ROTTEN = 2;
-    
-    function _getPositions(GRID_TYPE) {
-        let positions = [];
-        for (let i = 0; i < rcount; i++) {
-            for (let j = 0; j < ccount; j++) {
-                if (grid[i][j] === GRID_TYPE) {
-                    positions.push([i, j]);
-                }
-            }
+var orangesRotting = function (grid) {
+  // Given both m & n >= 1.
+  const m = grid.length;
+  const n = grid[0].length;
+  const EMPTY = 0,
+    FRESH = 1,
+    ROTTEN = 2;
+
+  function getNeighbours(r, c) {
+    return [
+      [-1, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1],
+    ]
+      .map(([dr, dc]) => [r + dr, c + dc])
+      .filter(([rn, cn]) => rn >= 0 && rn < m && cn >= 0 && cn < n);
+  }
+
+  // Start BFS from all the rotten oranges.
+  let numFresh = 0;
+  let level = [];
+  for (let r = 0; r < m; r++) {
+    for (let c = 0; c < n; c++) {
+      if (grid[r][c] === FRESH) {
+        numFresh++;
+      } else if (grid[r][c] === ROTTEN) {
+        level.push([r, c]);
+      }
+    }
+  }
+
+  // Return early if the grid doesn't contain any fresh oranges.
+  if (numFresh === 0) return 0;
+
+  let minutes = 0;
+  while (level.length > 0) {
+    minutes++;
+    const nextLevel = [];
+    for (const [r, c] of level) {
+      for (const [rn, cn] of getNeighbours(r, c)) {
+        if (grid[rn][cn] === FRESH) {
+          grid[rn][cn] = ROTTEN;
+          numFresh--;
+          if (numFresh === 0) return minutes;
+          nextLevel.push([rn, cn]);
         }
-        return positions;
+      }
     }
-    
-    // BFS the grid
-    // have to check only 4-directional neighbours
-    const offsets = [[-1,0], [0,1], [1,0], [0,-1]];
-    
-    let minutes = 0;
-    let freshPositions = _getPositions(FRESH);
-    if (freshPositions.length === 0) {
-        return minutes;
-    }
-    
-    let rottenPositions = _getPositions(ROTTEN); 
-    while (rottenPositions.length > 0) {
-        let newRottenPositions = [];
-        for (const [i, j] of rottenPositions) {
-            for (const [di, dj] of offsets) {
-                const newi = i+di, newj = j+dj;
-                // check boundaries
-                if (newi >= 0 && newi < rcount && newj >= 0 && newj < ccount) {
-                    if (grid[newi][newj] === FRESH) {
-                        grid[newi][newj] = ROTTEN;
-                        newRottenPositions.push([newi, newj]);
-                    }
-                }
-            }
-        }
-        
-        rottenPositions = newRottenPositions;
-        minutes++;
-    }
-    
-    // we exit the loop at minute i, i.e. there aren't any more rotten oranges at this point
-    // this means at minute i-1, we processed all the rotten oranges
-    
-    freshPositions = _getPositions(FRESH);
-    if (freshPositions.length > 0) {
-        return -1;
-    } else {
-        return minutes-1;
-    }
-    
+    level = nextLevel;
+  }
+  return -1;
 };
