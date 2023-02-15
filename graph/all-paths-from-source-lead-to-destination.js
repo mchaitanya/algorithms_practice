bb86aac491cc3218456d76aabb300245fc478657
mchaitanya <chaitanyaaa@gmail.com>
2023-02-15 11:34:22 +0530

    All Paths from Source Lead to Destination - detect cycles with back edges

--- graph/all-paths-from-source-lead-to-destination.js
+++ graph/all-paths-from-source-lead-to-destination.js
@@ -8,46 +8,76 @@
  * @return {boolean}
  */
 var leadsToDestination = function (n, edges, source, destination) {
-  // Store outgoing edges for each node in the map.
-  const map = new Map();
-  for (let i = 0; i < edges.length; i++) {
-    const v = edges[i][0];
-    if (!map.has(v)) map.set(v, []);
-    map.get(v).push(i);
+  // Convert edges into an adjacency list.
+  const al = new Array(n).fill(0).map(() => []);
+  for (const [u, v] of edges) {
+    al[u].push(v);
   }
 
-  // Keep track of edges we've seen.
-  const seen = new Array(edges.length).fill(false);
+  const WHITE = 0,
+    GRAY = 1,
+    BLACK = 2;
+  const status = new Array(n).fill(0);
 
-  // Memoize results to avoid repetitive work.
   const memo = new Array(n).fill(null);
-  // Return true if all paths from v end in the destination.
+  // Return true if all paths from v end in destination.
   function dfs(v) {
     if (memo[v] != null) return memo[v];
-    // If there aren't any outgoing edges from this node, it should be the destination.
-    if (!map.has(v)) {
-      memo[v] = v === destination;
-      return v === destination;
+    if (al[v].length === 0) {
+      return (memo[v] = v === destination);
     }
-    // Try each edge and backtrack.
-    for (const i of map.get(v)) {
-      // If the edge has been seen, that means the graph contains a cycle.
-      if (seen[i]) {
-        memo[v] = false;
-        return false;
-      }
-
-      const vn = edges[i][1];
-      seen[i] = true;
-      if (!dfs(vn)) {
-        memo[v] = false;
-        return false;
-      }
-      seen[i] = false;
+    status[v] = GRAY;
+    for (const vn of al[v]) {
+      // This is a back edge. Therefore, it creates a cycle.
+      if (status[vn] === GRAY) return (memo[v] = false);
+      // If an edge going out from v does not end in destination, return false.
+      if (!dfs(vn)) return (memo[v] = false);
     }
-
-    memo[v] = true;
-    return true;
+    status[v] = BLACK;
+    return (memo[v] = true);
   }
   return dfs(source);
+
+  //   // Store outgoing edges for each node in the map.
+  //   const map = new Map();
+  //   for (let i = 0; i < edges.length; i++) {
+  //     const v = edges[i][0];
+  //     if (!map.has(v)) map.set(v, []);
+  //     map.get(v).push(i);
+  //   }
+
+  //   // Keep track of edges we've seen.
+  //   const seen = new Array(edges.length).fill(false);
+
+  //   // Memoize results to avoid repetitive work.
+  //   const memo = new Array(n).fill(null);
+  //   // Return true if all paths from v end in the destination.
+  //   function dfs(v) {
+  //     if (memo[v] != null) return memo[v];
+  //     // If there aren't any outgoing edges from this node, it should be the destination.
+  //     if (!map.has(v)) {
+  //       memo[v] = v === destination;
+  //       return v === destination;
+  //     }
+  //     // Try each edge and backtrack.
+  //     for (const i of map.get(v)) {
+  //       // If the edge has been seen, that means the graph contains a cycle.
+  //       if (seen[i]) {
+  //         memo[v] = false;
+  //         return false;
+  //       }
+
+  //       const vn = edges[i][1];
+  //       seen[i] = true;
+  //       if (!dfs(vn)) {
+  //         memo[v] = false;
+  //         return false;
+  //       }
+  //       seen[i] = false;
+  //     }
+
+  //     memo[v] = true;
+  //     return true;
+  //   }
+  //   return dfs(source);
 };
