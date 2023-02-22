67a99e1f48807ab8ef9b9db77e8758f6cf96b112
mchaitanya <chaitanyaaa@gmail.com>
2023-02-22 19:59:53 +0530

    Binary Tree Inorder Traversal - different iterative solution

--- tree/binary-tree-inorder-traversal.js
+++ tree/binary-tree-inorder-traversal.js
@@ -13,22 +13,72 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
-  // Iterative with a stack.
   if (root == null) return [];
+
+  // Consider the recursive function
+  // dfs(node) {
+  //     dfs(node.left);
+  //     vals.push(node.val);
+  //     dfs(node.right);
+  // }
+
+  // Idea from:
+  // https://leetcode.com/problems/binary-tree-postorder-traversal/solutions/45559/my-accepted-code-with-explaination-does-anyone-have-a-better-idea/comments/45086
   const vals = [];
-  const stack = [[root, false]]; // Second value indicates if the left child has been seen.
+  const stack = [root];
+  let lastPopped;
   while (stack.length > 0) {
-    const [node, leftSeen] = stack[stack.length - 1];
-    if (node.left != null && !leftSeen) {
-      stack[stack.length - 1][1] = true;
-      stack.push([node.left, false]);
+    const top = stack[stack.length - 1];
+    if (
+      top.left != null &&
+      top.left !== lastPopped &&
+      top.right !== lastPopped
+    ) {
+      stack.push(top.left);
+    } else if (top.right != null && top.right !== lastPopped) {
+      // top.left is null / top.left was just popped.
+      vals.push(top.val);
+      stack.push(top.right);
     } else {
+      // top.left is null & top.right is null / one of them isn't null & it was just popped.
       stack.pop();
-      vals.push(node.val);
-      if (node.right != null) stack.push([node.right, false]);
+      lastPopped = top;
+      if (top.right == null) vals.push(top.val);
     }
   }
   return vals;
+
+  // if (root == null) return [];
+  // const vals = [];
+  // let curr = root;
+  // const stack = [];
+  // while (curr != null || stack.length > 0) {
+  //     while (curr != null) {
+  //         stack.push(curr);
+  //         curr = curr.left;
+  //     }
+  //     curr = stack.pop();
+  //     vals.push(curr.val);
+  //     curr = curr.right;
+  // }
+  // return vals;
+
+  // // Iterative with a stack.
+  // if (root == null) return [];
+  // const vals = [];
+  // const stack = [[root, false]]; // Second value indicates if the left child has been seen.
+  // while (stack.length > 0) {
+  //     const [node, leftSeen] = stack[stack.length-1];
+  //     if (node.left != null && !leftSeen) {
+  //         stack[stack.length-1][1] = true;
+  //         stack.push([node.left, false]);
+  //     } else {
+  //         stack.pop();
+  //         vals.push(node.val);
+  //         if (node.right != null) stack.push([node.right, false]);
+  //     }
+  // }
+  // return vals;
 
   // // Recursive
   // const vals = [];
