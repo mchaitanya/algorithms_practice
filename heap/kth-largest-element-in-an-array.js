e33ad286e0a00a22a5d6b3c346df77c1ab61c5c8
mchaitanya <chaitanyaaa@gmail.com>
2023-02-25 22:05:15 +0530

    Re-solve "Kth Largest Element in an Array"

--- heap/kth-largest-element-in-an-array.js
+++ heap/kth-largest-element-in-an-array.js
@@ -5,61 +5,82 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
-    const heap = new Heap((p, c) => p <= c); // min heap
-    for (let n of nums) {
-        if (heap.vals.length < k) {
-            heap.add(n);
-        } else if (n > heap.vals[0]) {
-            heap.extract();
-            heap.add(n);
-        }
+var findKthLargest = function (nums, k) {
+  // // Solve with a max-heap.
+  // const heap = new Heap(nums, (p,c) => p >= c);
+  // let numExtract = k-1; // Extract k-1 numbers. Then the top number will be the kth largest.
+  // while (numExtract > 0) {
+  //     heap.extract();
+  //     numExtract--;
+  // }
+  // return heap.vals[0];
+
+  // Solve with a min-heap.
+  const heap = new Heap([], (p, c) => p <= c);
+  for (const n of nums) {
+    if (heap.vals.length < k) {
+      heap.add(n);
+    } else if (n > heap.vals[0]) {
+      heap.extract();
+      heap.add(n);
     }
-    return heap.vals[0];
+  }
+  return heap.vals[0];
+
+  // // Solve with sorting.
+  // nums.sort((n1, n2) => n2-n1);
+  // return nums[k-1];
 };
 
 class Heap {
-    constructor(comparatorFn) {
-        this.vals = [];
-        this.cmpFn = comparatorFn;
+  constructor(vals, cmpFn) {
+    this.vals = vals;
+    this.cmp = cmpFn;
+    this.heapify();
+  }
+
+  add(val) {
+    this.vals.push(val);
+    this.bubble(this.vals.length - 1, "up");
+  }
+
+  extract() {
+    const top = this.vals.shift();
+    if (this.vals.length > 1) {
+      this.vals.unshift(this.vals.pop());
+      this.bubble(0, "down");
     }
-    
-    add(val) {
-        this.vals.push(val);
-        this.bubble(this.vals.length-1, 'up');
+    return top;
+  }
+
+  heapify() {
+    for (let i = this.vals.length - 1; i >= 0; i--) {
+      this.bubble(i, "down");
     }
-    
-    extract() {
-        const rootVal = this.vals.shift();
-        const lastVal = this.vals.pop();
-        if (lastVal != null) {
-            this.vals.unshift(lastVal);
-            this.bubble(0, 'down');
-        }
-        return rootVal;
+  }
+
+  bubble(i, dir) {
+    const pi = dir === "down" ? i : Math.floor((i - 1) / 2);
+    if (pi < 0) return;
+    const len = this.vals.length;
+    const li = 2 * pi + 1,
+      ri = 2 * pi + 2;
+    if (li >= len) return;
+    const pval = this.vals[pi],
+      lval = this.vals[li],
+      rval = this.vals[ri];
+    if (!this.cmp(pval, lval) || (ri < len && !this.cmp(pval, rval))) {
+      if (ri < len && this.cmp(rval, lval)) {
+        this.swapThenBubble(pi, ri, dir);
+      } else {
+        this.swapThenBubble(pi, li, dir);
+      }
     }
-    
-    bubble(ix, dir) {
-        const px = (dir === 'down' ? ix: Math.floor((ix-1)/2));
-        const pval = this.vals[px];
-        if (px < 0) return;
-        
-        const len = this.vals.length;
-        const lx = 2*px + 1, lval = this.vals[lx];
-        const rx = 2*px + 2, rval = this.vals[rx];
-        if ((lx < len && !this.cmpFn(pval, lval)) || (rx < len && !this.cmpFn(pval, rval))) {
-            if (rx < len && this.cmpFn(rval, lval)) {
-                this.swapThenRecurse(px, rx, dir);
-            } else if (lx < len) {
-                this.swapThenRecurse(px, lx, dir);
-            }
-        }
-    }
-    
-    swapThenRecurse(px, cx, dir) {
-        [this.vals[px], this.vals[cx]] = [this.vals[cx], this.vals[px]];
-        const nextIdx = (dir === 'up' ? px : cx);
-        this.bubble(nextIdx, dir);
-    }
-    
+  }
+
+  swapThenBubble(pi, ci, dir) {
+    [this.vals[pi], this.vals[ci]] = [this.vals[ci], this.vals[pi]];
+    const i = dir === "up" ? pi : ci;
+    this.bubble(i, dir);
+  }
 }
