bb5f40ff35bf1234abaeaae170554423f5c91f8f
mchaitanya <chaitanyaaa@gmail.com>
2023-03-03 13:07:40 +0530

    Re-solve "Coin Change"

--- dynamic-programming/coin-change.js
+++ dynamic-programming/coin-change.js
@@ -5,34 +5,36 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    const memo = Array(10000).fill(undefined);
-    memo[0] = 0;
-    
-    (function _computeMinCoinCount(amount) {
-        if (amount < 0) {
-            return -1;
-        }
-        
-        // check the memo array
-        if (memo[amount] !== undefined) {
-            return memo[amount];
-        }
+var coinChange = function (coins, amount) {
+  // BOTTOM-UP
+  const coinsReq = new Array(amount + 1).fill(0);
+  for (let i = 1; i <= amount; i++) {
+    let min = Infinity;
+    for (const c of coins) {
+      if (i - c >= 0 && coinsReq[i - c] > -1) {
+        min = Math.min(min, coinsReq[i - c] + 1);
+      }
+    }
+    coinsReq[i] = min === Infinity ? -1 : min;
+  }
+  return coinsReq[amount];
 
-        let result = Number.MAX_VALUE;
-        for (let coin of coins) {
-            let subResult = _computeMinCoinCount(amount - coin);
-            if (subResult > -1 && (subResult + 1 < result)) {
-                result = subResult + 1;
-            }
-        }
-        
-        // store the result in the memo array
-        memo[amount] = (result < Number.MAX_VALUE ? result : -1);
-        return memo[amount];
-        
-    })(amount);
-    
-    return memo[amount];
-    
+  // TOP-DOWN
+  // const memo = new Map();
+  // function coinsReq(amount) {
+  //     if (amount === 0) return 0;
+  //     if (memo.has(amount)) return memo.get(amount);
+
+  //     let result = -1;
+  //     for (const c of coins) {
+  //         if (amount < c) continue;
+  //         const sub = coinsReq(amount-c);
+  //         if (sub > -1) {
+  //             result = result === -1 ? sub+1 : Math.min(result, sub+1);
+  //         }
+  //     }
+  //     memo.set(amount, result);
+  //     return result;
+  // }
+  // return coinsReq(amount);
 };
