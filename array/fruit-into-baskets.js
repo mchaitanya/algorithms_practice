e0050e69bac925818414aac316439606a72c7720
mchaitanya <chaitanyaaa@gmail.com>
2023-03-06 20:00:24 +0530

    Re-solve "Fruit Into Baskets"

--- array/fruit-into-baskets.js
+++ array/fruit-into-baskets.js
@@ -4,32 +4,30 @@
  * @param {number[]} tree
  * @return {number}
  */
-var totalFruit = function(tree) {
-    // essentially find length of the longest subarray that contains only 2 distinct elements
-    let maxLen = 0;
-    let start = 0, end = 0;
-    const distinct = new Map();
-    
-    while (start < tree.length && end < tree.length) {
-        // invariant: window contains <= 2 types of tree
-        if (distinct.size < 2 || distinct.has(tree[end])) {
-            let count = distinct.get(tree[end]) || 0;
-            distinct.set(tree[end], count + 1);
-            end++; // end now points to next index to process
-            maxLen = Math.max(maxLen, end-start);
-        } else {
-            // distinct.size === 2 and tree[end] isn't in it
-            // advance start till distinct.size drops to 1
-            let count = distinct.get(tree[start]);
-            if (count > 1) {
-                distinct.set(tree[start], count-1);
-            } else {
-                distinct.delete(tree[start]);
-            }
-            start++;
-        }
+var totalFruit = function (tree) {
+  // Must pick exactly one fruit from every tree => We're looking for a subarray of fruits
+  // 2 baskets each holding only 1 type of fruit => Subarray can contain only 2 distinct types.
+  // Problem => Return longest subarray of fruits containing 2 distinct elements.
+  // Sliding window - Window contains <= 2 distinct elements
+  let maxFruits = 0;
+  for (
+    let map = new Map(), left = 0, right = 0;
+    right < fruits.length;
+    right++
+  ) {
+    // Add fruits[end] into basket
+    const rcount = map.get(fruits[right]) || 0;
+    map.set(fruits[right], rcount + 1);
+    while (left <= right && map.size > 2) {
+      const lcount = map.get(fruits[left]);
+      if (lcount > 1) {
+        map.set(fruits[left], lcount - 1);
+      } else {
+        map.delete(fruits[left]);
+      }
+      left++;
     }
-    
-    return maxLen;
-    
+    maxFruits = Math.max(maxFruits, right - left + 1);
+  }
+  return maxFruits;
 };
