a9e10e08dd0cd9d0041516ff69bb3bb286f6e823
mchaitanya <chaitanyaaa@gmail.com>
2023-03-16 11:34:23 +0530

    Longest Uncommon Subsequence I

---
+++ string/longest-uncommon-subsequence-i.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-uncommon-subsequence-i/
+// tags - string
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+  return a === b ? -1 : Math.max(a.length, b.length);
+};
