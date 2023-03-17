520587af00e7ede4130cf927f2a11402466a2b96
mchaitanya <chaitanyaaa@gmail.com>
2023-03-17 21:12:42 +0530

    Re-solve 'Count Univalue Subtrees'

--- tree/count-univalue-subtrees.js
+++ tree/count-univalue-subtrees.js
@@ -13,23 +13,37 @@
  * @return {number}
  */
 var countUnivalSubtrees = function (root) {
-  if (root == null) return 0;
-  let count = 0;
-  // Return whether the subtree rooted at node is univalued.
-  function dfs(node) {
-    // node shouldn't be null.
-    let isUnival = true;
-    if (node.left != null) {
-      const isLeftUnival = dfs(node.left);
-      isUnival &&= node.val === node.left.val && isLeftUnival;
-    }
-    if (node.right != null) {
-      const isRightUnival = dfs(node.right);
-      isUnival &&= node.val === node.right.val && isRightUnival;
-    }
-    if (isUnival) count++;
-    return isUnival;
-  }
-  dfs(root);
-  return count;
+  let numUnival = 0;
+  (function isUnival(node) {
+    if (node == null) return false;
+    const leftUnival = isUnival(node.left);
+    const rightUnival = isUnival(node.right);
+    const nodeUnival =
+      (node.left == null || (node.val === node.left.val && leftUnival)) &&
+      (node.right == null || (node.val === node.right.val && rightUnival));
+    if (nodeUnival) numUnival++;
+    return nodeUnival;
+  })(root);
+
+  return numUnival;
+
+  // if (root == null) return 0;
+  // let count = 0;
+  // // Return whether the subtree rooted at node is univalued.
+  // function dfs(node) {
+  //   // node shouldn't be null.
+  //   let isUnival = true;
+  //   if (node.left != null) {
+  //     const isLeftUnival = dfs(node.left);
+  //     isUnival &&= node.val === node.left.val && isLeftUnival;
+  //   }
+  //   if (node.right != null) {
+  //     const isRightUnival = dfs(node.right);
+  //     isUnival &&= node.val === node.right.val && isRightUnival;
+  //   }
+  //   if (isUnival) count++;
+  //   return isUnival;
+  // }
+  // dfs(root);
+  // return count;
 };
