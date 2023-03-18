85bf0046d72fe313aefbcac4251caaf53f7cf596
mchaitanya <chaitanyaaa@gmail.com>
2023-03-18 17:20:34 +0530

    Re-solve "Squares of a Sorted Array"

--- array/squares-of-a-sorted-array.js
+++ array/squares-of-a-sorted-array.js
@@ -4,8 +4,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    const result = nums.map(n => n*n);
-    result.sort((n1, n2) => n1-n2);
-    return result;
+var sortedSquares = function (nums) {
+  nums = nums.map((n) => Math.abs(n));
+  const squared = new Array(nums.length);
+  for (let i = 0, j = nums.length - 1, k = nums.length - 1; k >= 0; k--) {
+    if (nums[i] > nums[j]) {
+      squared[k] = nums[i] * nums[i];
+      i++;
+    } else {
+      squared[k] = nums[j] * nums[j];
+      j--;
+    }
+  }
+  return squared;
+
+  // const result = nums.map(n => n*n);
+  // result.sort((n1, n2) => n1-n2);
+  // return result;
 };
