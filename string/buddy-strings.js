59dcf6c8d81a72979f64531a8ce847bba25f577a
mchaitanya <chaitanyaaa@gmail.com>
2023-03-19 20:46:21 +0530

    Re-solve "Buddy Strings"

--- string/buddy-strings.js
+++ string/buddy-strings.js
@@ -8,36 +8,30 @@
 var buddyStrings = function (s, goal) {
   if (s.length !== goal.length) return false;
 
-  const distinct = new Set(s);
-  // If there are repeated chars, we can swap them.
-  if (s === goal) return s.length !== distinct.size;
-
   let diff1, diff2;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === goal[i]) continue;
-    if (diff1 == undefined) {
-      diff1 = i;
-    } else if (diff2 == undefined) {
-      diff2 = i;
-    } else {
-      return false;
+    if (s[i] !== goal[i]) {
+      if (diff1 == null) {
+        diff1 = i;
+      } else if (diff2 == null) {
+        diff2 = i;
+      } else {
+        // The strings differ at 3 indexes. Impossible to fix by swapping 2.
+        return false;
+      }
     }
   }
 
-  // diff2 will be undefined if s & goal only differ in one position.
-  return (
-    diff2 !== undefined && s[diff1] === goal[diff2] && s[diff2] === goal[diff1]
-  );
-
-  // const letters = s.split('');
-  // for (let i = 0; i < s.length; i++) {
-  //     for (let j = i+1; j < s.length; j++) {
-  //         [letters[i], letters[j]] = [letters[j], letters[i]]; // Swap the letters.
-  //         if (letters.join('') === goal) {
-  //             return true;
-  //         }
-  //         [letters[i], letters[j]] = [letters[j], letters[i]]; // Swap them back.
-  //     }
-  // }
-  // return false;
+  // 3 cases
+  // - 0 diffs
+  // - 1 diff
+  // - 2 diffs
+  if (diff1 == null) {
+    const set = new Set(s);
+    return set.size < s.length; // There are repeated characters we can swap producing no effect.
+  } else if (diff2 == null) {
+    return false;
+  } else {
+    return s[diff1] === goal[diff2] && s[diff2] === goal[diff1];
+  }
 };
