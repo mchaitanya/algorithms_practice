6bea3b078358d6298dbab4400497d2f6a72f2da9
mchaitanya <chaitanyaaa@gmail.com>
2023-03-26 13:16:45 +0530

    Re-solve "Majority Element II"

--- array/majority-element-ii.js
+++ array/majority-element-ii.js
@@ -4,14 +4,52 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var majorityElement = function(nums) {
-    const map = new Map();
-    for (let n of nums) {
-        let count = map.get(n) || 0;
-        map.set(n, count+1);
+var majorityElement = function (nums) {
+  // Boyer-Moore from LC
+  let candidate1, candidate2;
+  for (let i = 0, count1 = 0, count2 = 0; i < nums.length; i++) {
+    if (nums[i] !== candidate1 && nums[i] !== candidate2) {
+      if (count1 === 0) {
+        candidate1 = nums[i];
+        count1 = 1;
+      } else if (count2 === 0) {
+        candidate2 = nums[i];
+        count2 = 1;
+      } else {
+        count1--;
+        count2--;
+      }
+    } else {
+      if (nums[i] === candidate1) count1++;
+      if (nums[i] === candidate2) count2++;
     }
-    
-    return Array.from(map.entries())
-            .filter(([_, count]) => count > Math.floor(nums.length/3))
-            .map(([n, _]) => n);
+  }
+
+  const result = [];
+  const threshold = Math.floor(nums.length / 3) + 1;
+  for (let i = 0, count1 = 0, count2 = 0; i < nums.length; i++) {
+    // Check candidate1.
+    if (nums[i] === candidate1) {
+      count1++;
+      if (count1 === threshold) result.push(candidate1);
+    }
+    // Check candidate2.
+    if (nums[i] === candidate2) {
+      count2++;
+      if (count2 === threshold) result.push(candidate2);
+    }
+  }
+  return result;
+
+  // const threshold = Math.floor(nums.length/3);
+  // const map = new Map();
+  // const result = [];
+  // for (const num of nums) {
+  //     const count = map.get(num) || 0;
+  //     map.set(num, count+1);
+  //     if (count === threshold) {
+  //         result.push(num);
+  //     }
+  // }
+  // return result;
 };
