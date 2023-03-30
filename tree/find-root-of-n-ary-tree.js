ce1b5a9dd9d177e61ee2b86d374ea290d663c9df
mchaitanya <chaitanyaaa@gmail.com>
2023-03-30 21:22:16 +0530

    Re-solve "Find Root of N-Ary Tree"

--- tree/find-root-of-n-ary-tree.js
+++ tree/find-root-of-n-ary-tree.js
@@ -12,16 +12,31 @@
  * @param {Node[]} tree
  * @return {Node}
  */
-var findRoot = function(tree) {
-    // the root doesn't have any parent
-    const set = new Set(tree);
-    for (let node of tree) {
-        for (let child of node.children) {
-            set.delete(child);
-        }
+var findRoot = function (tree) {
+  let nodeValsSum = 0,
+    childValsSum = 0;
+  for (const node of tree) {
+    nodeValsSum += node.val;
+    for (const child of node.children) {
+      childValsSum += child.val;
     }
-    
-    // there should be one node left in the set - the root
-    return [...set][0];
-    
+  }
+
+  const rootVal = nodeValsSum - childValsSum;
+  for (const node of tree) {
+    if (node.val === rootVal) return node;
+  }
+  // Shouldn't reach here.
+
+  //   // the root doesn't have any parent
+  //   const set = new Set(tree);
+  //   for (let node of tree) {
+  //     for (let child of node.children) {
+  //       set.delete(child);
+  //     }
+  //   }
+
+  //   // there should be one node left in the set - the root
+  //   const { value: root } = set.values().next();
+  //   return root;
 };
