50dbd22e69693b00b3954a2a4a53f80a99d66241
mchaitanya <chaitanyaaa@gmail.com>
2023-04-07 20:40:16 +0530

    Find Bottom Left Tree Value - simpler solution

--- tree/find-bottom-left-tree-value.js
+++ tree/find-bottom-left-tree-value.js
@@ -13,17 +13,26 @@
  * @return {number}
  */
 var findBottomLeftValue = function (root) {
-  // Traverse the tree level-order.
   let result;
-  let curr = [root];
-  while (curr.length > 0) {
-    result = curr[0];
-    const next = [];
-    for (let node of curr) {
-      if (node.left) next.push(node.left);
-      if (node.right) next.push(node.right);
-    }
-    curr = next;
+  const queue = [root];
+  while (queue.length > 0) {
+    result = queue.shift();
+    if (result.right) queue.push(result.right);
+    if (result.left) queue.push(result.left);
   }
   return result.val;
+
+  // // Traverse the tree level-order.
+  // let result;
+  // let curr = [root];
+  // while (curr.length > 0) {
+  //     result = curr[0];
+  //     const next = [];
+  //     for (let node of curr) {
+  //         if (node.left) next.push(node.left);
+  //         if (node.right) next.push(node.right);
+  //     }
+  //     curr = next;
+  // }
+  // return result.val;
 };
