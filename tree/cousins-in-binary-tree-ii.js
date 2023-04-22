3fb171862a59bd8d5aeab10ea51197d87fea60c4
mchaitanya <chaitanyaaa@gmail.com>
2023-04-22 18:17:03 +0530

    Small update to "Cousins in Binary Tree II"

--- tree/cousins-in-binary-tree-ii.js
+++ tree/cousins-in-binary-tree-ii.js
@@ -9,6 +9,14 @@
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {TreeNode}
  */
@@ -17,24 +25,23 @@ var replaceValueInTree = function (root) {
   root.val = 0;
   let level = [root];
   while (level.length > 0) {
+    const next = [];
     let levelSum = 0;
     for (const node of level) {
       const siblingSum = (node.left?.val || 0) + (node.right?.val || 0);
       levelSum += siblingSum;
-      if (node.left) node.left.val = -siblingSum;
-      if (node.right) node.right.val = -siblingSum;
-    }
-
-    const next = [];
-    for (const node of level) {
       if (node.left) {
-        node.left.val += levelSum;
+        node.left.val = -siblingSum;
         next.push(node.left);
       }
       if (node.right) {
-        node.right.val += levelSum;
+        node.right.val = -siblingSum;
         next.push(node.right);
       }
+    }
+
+    for (const node of next) {
+      node.val += levelSum;
     }
     level = next;
   }
