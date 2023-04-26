c23d58e10716cf5b5f1fee094c44668eb20a9e83
mchaitanya <chaitanyaaa@gmail.com>
2023-04-26 11:34:54 +0530

    Re-solve "Long Pressed Name"

--- string/long-pressed-name.js
+++ string/long-pressed-name.js
@@ -5,32 +5,51 @@
  * @param {string} typed
  * @return {boolean}
  */
-var isLongPressedName = function(name, typed) {
-    function _getCharGroups(s) {
-        const result = [];
-        for (let i = 0, count = 0; i < s.length; i++) {
-            count++;
-            if (i === s.length-1 || s[i] !== s[i+1]) {
-                result.push([s[i], count])
-                count = 0;
-            }
-        }
-        return result;
+var isLongPressedName = function (name, typed) {
+  function _getCharGroups(s) {
+    const result = [];
+    for (let i = 0, count = 0; i < s.length; i++) {
+      count++;
+      if (i === s.length - 1 || s[i] !== s[i + 1]) {
+        result.push([s[i], count]);
+        count = 0;
+      }
     }
-    
-    const nameGroups = _getCharGroups(name);
-    const typedGroups = _getCharGroups(typed);
-    if (nameGroups.length !== typedGroups.length) {
-        return false;
+    return result;
+  }
+
+  const nameGroups = _getCharGroups(name);
+  const typedGroups = _getCharGroups(typed);
+  if (nameGroups.length !== typedGroups.length) {
+    return false;
+  }
+
+  for (let i = 0; i < nameGroups.length; i++) {
+    const [nameChar, nameCount] = nameGroups[i];
+    const [typedChar, typedCount] = typedGroups[i];
+    if (nameChar !== typedChar || nameCount > typedCount) {
+      return false;
     }
-    
-    for (let i = 0; i < nameGroups.length; i++) {
-        const [nameChar, nameCount] = nameGroups[i];
-        const [typedChar, typedCount] = typedGroups[i];
-        if (nameChar !== typedChar || nameCount > typedCount) {
-            return false;
-        }
-    }
-    return true;
-    
+  }
+  return true;
+
+  // for (let i = 0, j = 0; i < name.length || j < typed.length; i++, j++) {
+  //     if (i === name.length || j === typed.length) return false;
+  //     if (name[i] !== typed[j]) return false;
+
+  //     let nameCount = 1;
+  //     while (i+1 < name.length && name[i+1] === name[i]) {
+  //         nameCount++;
+  //         i++;
+  //     }
+
+  //     let typedCount = 1;
+  //     while (j+1 < typed.length && typed[j+1] === typed[j]) {
+  //         typedCount++;
+  //         j++;
+  //     }
+
+  //     if (nameCount > typedCount) return false;
+  // }
+  // return true;
 };
